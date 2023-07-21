f387067087d6190980a9e7fe38d0f004dc447443
Kateryna Len <lenkaterynna057@gmail.com>
2023-07-21 11:28:35 +0300

    create mobile burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-const box = document.querySelector('.box');
+const block = document.getElementById('mobile');
+
+function myFunction() {
+  if (block.style.display === 'block') {
+    document.getElementById('mobile').style.display = 'none';
+  } else {
+    document.getElementById('mobile').style.display = 'block';
+  }
+}
